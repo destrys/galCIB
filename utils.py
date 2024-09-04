9f717c9ebc3925ae46f0a202e1a04896adb141e4
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2024-09-04 18:34:04 -0400

    added functions from serra 2014

--- utils.py
+++ utils.py
@@ -5,7 +5,93 @@ Author: Tanveer Karim
 Last Updated: 4 September 2024
 """
 
-def cibXgal_pk_2h():
+from scipy.integrate import simpson
+
+def B(nu, T):
+    """
+    Returns Plancks blackbody function as a function
+    of freq nu and temperature T.
+    """
+    
+    const_term = 2 * planck_const_h/speed_of_light**2
+    exp_term = 1/(np.exp((planck_const_h * nu)/(boltzmann_kb * T))-1)
+    return const_term * nu**3 * exp_term
+    
+
+def Theta(nu, z, beta, Td, gamma):
+    """
+    Returns the SED model of CIB-emitting haloes
+    
+    Arguments:
+        nu : frequency
+        z : redshift
+        beta : emissivity index, related to physical nature of dust
+        Td : effective dust temperature
+        gamma : power index 
+    """
+    
+    res = np.zeros_like(nu)
+    flag = (nu < nu0) # freq below pivot freq nu0
+     
+    res[flag] = nu**beta * B(nu, Td)
+    res[~flag] = nu**(-1 * gamma)
+    
+    return res 
+
+def shang12(L0, z):
+    """
+    Returns the specific IR luminosity at frequency nu
+    using the Shang et al. 2012 (1109.1522) model.
+    """
+    
+    res = L0 * (1 + z)**(3.6) * Sigma(M) * Theta ((1+z) * nu)
+    return res
+    return 0 #FIXME
+
+def L_nuprime_z(M, z, model_name):
+    """"
+    Returns the specific IR luminosity at frequency nu.
+    """
+    
+    # call specific model and return value 
+    return 0 #FIXME
+    
+
+def j_nuprime_z(nu, z, dM):
+    """
+    Returns the j_(1+z)nu (z), comoving emissivity at a
+    given frequency nu and redshift z.
+    
+    Arguments:
+        dM : spacing of the halo mass function
+    
+    Returns:
+        res : comoving emissivity value
+    """
+    
+    #integrand = dndM * L_nuprime_z(M, z)/4*np.pi
+    # res = simpson(y = integrand, x = dM)
+    #return res
+    return 0 #FIXME
+
+
+def cib_halo():
+    
+    
+
+def cibXgal_pk_2h(Pmm):
+    """
+    Returns the theory cross Pk of CIB and galaxy
+    
+    Arguments
+    ---------
+        Pmm : linear matter power spectrum
+    """
+    
+    bias_gal
+    bias_CIB
+    
+    P_cibXgal = bias_gal * bias_CIB * Pmm 
 
 def cibXgal_cell_2h():
     """"
