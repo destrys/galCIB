5e00435f6dae67201db9021ec55f3c84cd056145
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2024-09-04 17:43:41 -0400

    initialize utils.py

---
+++ utils.py
@@ -0,0 +1,23 @@
+"""
+Script repurposed from Abhishek Maniyar's DopplerCIB github. 
+
+Author: Tanveer Karim
+Last Updated: 4 September 2024
+"""
+
+def cibXgal_pk_2h():
+
+def cibXgal_cell_2h():
+    """"
+    Returns the theory cross C_ell of CIB and galaxy
+    """
+    
+    ucen = 
+    unfw = 
+    power = 
+    w1w2 = 
+    geo = 
+    fact = 
+    rest = 
+    
+    
