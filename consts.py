d16edcc65efc231546ef714c2d85ddbb2b29b9bf
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2024-09-19 11:28:15 -0400

    fixed redshift file errors

--- consts.py
+++ consts.py
@@ -5,6 +5,7 @@ Helper function and constants for analysis
 from astropy.cosmology import Planck18 as planck
 from astropy import constants as const
 import pandas as pd 
+import pickle 
 
 # global variables 
 planck_const_h = const.h
@@ -14,12 +15,13 @@ boltzmann_kb = const.k_B
 OmegaM = planck.Om0
 H0 = planck.H0
 
-##FIXME: define the redshift bins here to pre-calculate 
-## chi, dchi_dz
+# linear power spectrum
+with open('data/plin_unit_h.p', 'rb') as handle:
+    Plin = pickle.load(handle)
 
-
-P_mm = #FIXME: define P_mm from CAMB
-dchi_dz = speed_of_light/planck.H(zrange)
+# ##FIXME: define the redshift bins here to pre-calculate 
+# ## chi, dchi_dz
+# dchi_dz = speed_of_light/planck.H(zrange)
 
 # store all relevant galaxy information
 dict_gal = {}
@@ -28,10 +30,18 @@ dict_gal = {}
 dict_gal['ELG'] = {}
 
 dndz = pd.read_csv("data/gal/elg_fuji_pz_single_tomo.csv")
-dict_gal['ELG']['z'] = dndz['zrange'].values
-dict_gal['ELG']['pz'] = dndz['dndz'].values
+dict_gal['ELG']['z'] = dndz['Redshift_mid'].values
+dict_gal['ELG']['pz'] = dndz['pz'].values
 dict_gal['ELG']['mag_bias_alpha'] = 2.225
 
 # cosmology-related values
 dict_gal['ELG']['chi'] = planck.comoving_distance(dict_gal['ELG']['z'])
 dict_gal['ELG']['Hz'] = planck.H(dict_gal['ELG']['z'])
+
+# dict with ELG HOD properties based on 
+# Table 7 of Rocher et al. 2023
+dict_gal['ELG']['HOD'] = {}
+dict_gal['ELG']['HOD']['Ac'] = 0.1
+dict_gal['ELG']['HOD']['log10Mc'] = 11.64
+dict_gal['ELG']['HOD']['sigmaM'] = 0.30
+dict_gal['ELG']['HOD']['gamma'] = 5.47
