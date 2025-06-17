6cd569f3c9056fd2d1f76895dd318dfb630bc421
Tanveer Karim <tanveer.karim@cfa.harvard.edu>
2025-06-17 16:04:23 -0400

    Setup cosmology class

--- src/galCIB/__init__.py
+++ src/galCIB/__init__.py
@@ -1,10 +1,19 @@
 # src/galCIB/__init__.py
 
+from .cosmology import Cosmology
+
 # Remove all imports, leave empty or just __all__
-__all__ = [
-    "clustering",
-    "cosmology",
-    "consts",
-    "powerspectra",
-    "precalc",
-]
+# __all__ = [
+#     "clustering",
+#     "cosmology",
+#     "consts",
+#     "powerspectra",
+#     "precalc",
+# ]
+
+
+# from .survey import Survey
+# from .power import PowerSpectra
+
+__all__ = ['Cosmology']#, 'Survey', 'PowerSpectra']
+__version__ = "0.0.1"
---
+++ src/galCIB/cosmology/core.py
@@ -0,0 +1,156 @@
+"""
+This module defines the fixed cosmology used in the analysis.
+This is helpful to pass consistent values from and to CAMB and colossus.
+"""
+
+
+import numpy as np
+
+from colossus.cosmology import cosmology as cc
+from colossus.lss import mass_function
+
+
+class Cosmology:
+    def __init__(self, zs, ks, Mh, 
+                 colossus_cosmo_name = 'planck18',
+                 use_little_h = False):
+        
+        """
+        Returns Cosmology Class with user-defined choices.
+        
+        Args:
+            zs : redshift range
+            ks : k range 
+            Mh : Halo mass range
+            colossus_cosmo_name : str referring to built-in colossus cosmology
+            use_little_h : bool, True if using little h convention
+        """
+        
+        self.cosmo_name = colossus_cosmo_name
+        self.z = zs
+        self.k = ks
+        self.Mh = Mh
+        self.use_little_h = use_little_h
+        
+        self.pk_grid, self.hmf_grid, self.cosmo = self._load_colossus()
+        
+    
+    def _load_colossus(self):
+        """
+        Returns P_mm (k), Halo Mass Function and Halo Bias
+        from colossus. 
+        """
+        
+        cosmo = cc.setCosmology(self.cosmo_name)
+        
+        pk_grid = np.zeros((len(self.k),len(self.z)))
+        hmf_grid = np.zeros((len(self.Mh),len(self.z)))
+        
+        if self.use_little_h:
+            k_range = self.k * cosmo.h
+            Mh_range = self.Mh/cosmo.h
+        else: 
+            k_range = self.k
+            Mh_range = self.Mh
+        
+        for i in range(len(self.z)):
+            tmp_pk = cosmo.matterPowerSpectrum(k_range, 
+                                               self.z[i],
+                                               model = 'camb')
+            
+            # Convert dn/dlnM to dn/dlog_10M, using ln(10) factor. 
+            tmp_hmf = mass_function.massFunction(x=Mh_range, z=self.z[i],
+                                                     mdef='200m', model='tinker08',
+                                                     q_in='M', q_out='dndlnM') * np.log(10)
+            
+            # whether to convert HMF into h-less unit
+            if self.use_little_h:
+                pk_grid[:,i] = tmp_pk/cosmo.h**3 
+                hmf_grid[:,i] = tmp_hmf*cosmo.h**3
+            else:
+                pk_grid[:,i] = tmp_pk
+                hmf_grid[:,i] = tmp_hmf
+                
+        return pk_grid, hmf_grid, cosmo
+    
+    # def _calculate_prop(self):
+    #     """
+    #     Returns most used relevant cosmological properties 
+    #     that are used for C_ell and Pk calculations.
+        
+    #     Calculates the following:
+    #     1. comoving distance, chi(z)
+    #     2. H(z)
+    #     3. Omegab/OmegaM (z)
+    #     4. rho_crit (z)
+    #     5. mean density at z = 0
+    #     6. H(z)/(c * chi^2(z))
+    #     7. chi^2(z)
+    #     8. dchi/dz (z)
+        
+    #     """
+        
+    #     # comoving distance in units of Mpc
+    #     chi = np.zeros_like(self.z)
+    #     for i in range(len(self.z)):
+    #         chi[i] = self.cosmo.comovingDistance(0,
+    #                                              self.z[i])
+            
+    #     # H(z) 
+    #     Hz = self.cosmo(self.z)
+        
+        
+        
+    #     if self.use_little_h is False:
+    #         chi = chi/self.cosmo.h 
+            
+    def get_pk_grid(self):
+        """
+        Returns the precomputed P(k,z) grid.
+        """   
+        
+        return self.pk_grid
+    
+    def get_hmf_grid(self):
+        """
+        Returns precomputed HMF(z,Mh)
+        """
+        
+        return self.hmf_grid
+    
+    def get_k_range(self, use_little_h):
+        
+        if use_little_h:
+            res = self.k * self.cosmo.h
+        else:
+            res = self.k
+            
+        return res 
+    
+    def get_Mh_range(self, use_little_h):
+        
+        if use_little_h:
+            res = self.Mh/self.cosmo.h
+        else:
+            res = self.Mh 
+            
+        return res
+    
+    def save_grids(self, filename):
+        np.savez(filename, 
+                    pk=self.pk_grid, 
+                    z=self.z, k=self.k,
+                    hmf=self.hmf_grid,
+                    Mh=self.Mh)
+        
+    @classmethod
+    def load_grids(cls, filename):
+        data = np.load(filename)
+        instance = cls.__new__(cls)  # create instance without __init__
+        instance.pk_grid = data['pk']
+        instance.z = data['z']
+        instance.k = data['k']
+        instance.hmf = data['hmf']
+        instance.Mh = data['Mh']
+        
+        return instance
